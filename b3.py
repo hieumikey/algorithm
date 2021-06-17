ceb264d010cc8f1969dedee98395dce0e782c474
Pham Nhat Hieu <44352724+hieumikey@users.noreply.github.com>
2021-06-17 11:14:01 +0700

    Update b3.py

--- b3.py
+++ b3.py
@@ -10,18 +10,14 @@ with requests.Session() as s:
     print(p.status_code)
     r = s.get('https://00e57086a7747a5a96c58b6f4a88eb61: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@hieupn99.myshopify.com/admin/api/2021-04/customers.json')
     data = r.text
-    # with open('data.txt', 'w') as outfile:
-    #     json.dump(data, outfile)
-    #print(type(data))
     data = json.loads(data)
-    print(data['customers'][0]['id'])
-    customers = data['customers'][0]
-    with open('customertest.csv', 'w') as f:
-        for key in customers.keys():
-            f.write("%s,%s\n"%(key, customers[key]))
-    data_customer = pd.read_csv('customertest.csv').T
-    os.remove("customertest.csv")
-    print(type(data_customer))
-    data_customer.to_csv('customerapi.csv')
-    t = pd.read_csv('customerapi.csv')
-    print(t)
+    customers= []
+    for i in data['customers']:
+        customers.append(i)
+    print(type(customers[0]))
+    attr = customers[0].keys()
+    a_file = open("output.csv", "w")
+    dict_w = csv.DictWriter(a_file, attr)
+    dict_w.writeheader()
+    dict_w.writerows(customers)
+    a_file.close()
